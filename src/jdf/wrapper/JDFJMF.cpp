e5ea22b59e37697a979c74eee4318962b9ae8913
Rainer Prosi <Rainer.Prosi@heidelberg.com>
2008-03-06 08:17:20 +0000

    http://www.cip4.org/jira/browse/JDFC-50
    git-svn-id: http://svn.cip4.org/svnroot/trunk@3176 25ee8a6d-831b-0410-907a-ff8528d65d49

--- src/jdf/wrapper/JDFJMF.cpp
+++ src/jdf/wrapper/JDFJMF.cpp
@@ -2,7 +2,7 @@
 * The CIP4 Software License, Version 1.0
 *
 *
-* Copyright (c) 2001-2006 The International Cooperation for the Integration of 
+* Copyright (c) 2001-2008 The International Cooperation for the Integration of 
 * Processes in  Prepress, Press and Postpress (CIP4).  All rights 
 * reserved.
 *
@@ -279,7 +279,7 @@ namespace JDF{
 		SetTimeStamp();
 		AddNameSpace(WString::emptyStr,GetSchemaURL(1,1));
 		SetEnumVersion(getDefaultJDFVersion());
-
+        setXSIType(L"JMFRootMessage");
 		return true;
 	}
 	//////////////////////////////////////////////////////////////////////
