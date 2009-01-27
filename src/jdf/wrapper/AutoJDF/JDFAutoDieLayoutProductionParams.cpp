8bba900c4a959506ed7726089b2652842ec28ceb
Rainer Prosi <Rainer.Prosi@heidelberg.com>
2009-01-27 12:53:02 +0000

    update to JDF 1.4
    git-svn-id: http://svn.cip4.org/svnroot/trunk@3799 25ee8a6d-831b-0410-907a-ff8528d65d49

---
+++ src/jdf/wrapper/AutoJDF/JDFAutoDieLayoutProductionParams.cpp
@@ -0,0 +1,295 @@
+/*
+ * The CIP4 Software License, Version 1.0
+ *
+ *
+ * Copyright (c) 2001-2009 The International Cooperation for the Integration of 
+ * Processes in  Prepress, Press and Postpress (CIP4).  All rights 
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer. 
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution,
+ *    if any, must include the following acknowledgment:  
+ *       "This product includes software developed by the
+ *        The International Cooperation for the Integration of 
+ *        Processes in  Prepress, Press and Postpress (www.cip4.org)"
+ *    Alternately, this acknowledgment may appear in the software itself,
+ *    if and wherever such third-party acknowledgments normally appear.
+ *
+ * 4. The names "CIP4" and "The International Cooperation for the Integration of 
+ *    Processes in  Prepress, Press and Postpress" must
+ *    not be used to endorse or promote products derived from this
+ *    software without prior written permission. For written 
+ *    permission, please contact info@cip4.org.
+ *
+ * 5. Products derived from this software may not be called "CIP4",
+ *    nor may "CIP4" appear in their name, without prior written
+ *    permission of the CIP4 organization
+ *
+ * Usage of this software in commercial products is subject to restrictions. For
+ * details please consult info@cip4.org.
+  *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE INTERNATIONAL COOPERATION FOR
+ * THE INTEGRATION OF PROCESSES IN PREPRESS, PRESS AND POSTPRESS OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the The International Cooperation for the Integration 
+ * of Processes in Prepress, Press and Postpress and was
+ * originally based on software 
+ * copyright (c) 1999-2001, Heidelberger Druckmaschinen AG 
+ * copyright (c) 1999-2001, Agfa-Gevaert N.V. 
+ *  
+ * For more information on The International Cooperation for the 
+ * Integration of Processes in  Prepress, Press and Postpress , please see
+ * <http://www.cip4.org/>.
+ *  
+ * 
+ */
+
+//EndCopyRight
+
+
+///////////////////////////////////////////////////////////////////
+
+ 
+#include "jdf/wrapper/AutoJDF/JDFAutoDieLayoutProductionParams.h"
+#include "jdf/wrapper/JDFConvertingConfig.h"
+#include "jdf/wrapper/JDFRepeatDesc.h"
+#include "jdf/wrapper/JDFRefElement.h"
+namespace JDF{
+/*
+*********************************************************************
+class JDFAutoDieLayoutProductionParams : public JDFResource
+
+*********************************************************************
+*/
+/**
+* copy equivalance operator
+*/
+JDFAutoDieLayoutProductionParams& JDFAutoDieLayoutProductionParams::operator=(const KElement& other){
+	KElement::operator=(other);
+	if(!IsValid(ValidationLevel_Construct))
+		throw JDFException(L"Invalid constructor for JDFAutoDieLayoutProductionParams: "+other.GetNodeName());
+	return *this;
+};
+
+/////////////////////////////////////////////////////
+	bool JDFAutoDieLayoutProductionParams::IsAbstract()const{
+		return false;
+	}
+
+/**
+* typesafe validator utility - list of valid node names for this class 
+ * @return WString& comma separated list of valid node names
+ */
+
+	WString JDFAutoDieLayoutProductionParams::ValidNodeNames()const{
+	return L"*:,DieLayoutProductionParams";
+};
+
+bool JDFAutoDieLayoutProductionParams::ValidClass(EnumValidationLevel level) const {
+	if(!HasAttribute(atr_Class))
+		return !RequiredLevel(level);
+	return GetClass()==Class_Parameter;
+};
+
+bool JDFAutoDieLayoutProductionParams::init(){
+	bool bRet=JDFResource::init();
+	SetClass(Class_Parameter);
+	return bRet;
+};
+
+/* ******************************************************
+// Attribute Getter / Setter
+****************************************************** */
+
+
+/**
+ definition of optional attributes in the JDF namespace
+*/
+	WString JDFAutoDieLayoutProductionParams::OptionalAttributes()const{
+		return JDFResource::OptionalAttributes()+WString(L",Estimate,Position");
+};
+
+/**
+ typesafe validator
+*/
+	vWString JDFAutoDieLayoutProductionParams::GetInvalidAttributes(EnumValidationLevel level, bool bIgnorePrivate, int nMax)const {
+		vWString vAtts=JDFResource::GetInvalidAttributes(level,bIgnorePrivate,nMax);
+		int n=vAtts.size();
+		if(n>=nMax)
+			return vAtts;
+		if(!ValidEstimate(level)) {
+			vAtts.push_back(atr_Estimate);
+			if(++n>=nMax)
+				return vAtts;
+		};
+		if(!ValidPosition(level)) {
+			vAtts.push_back(atr_Position);
+			if(++n>=nMax)
+				return vAtts;
+		};
+		return vAtts;
+	};
+
+/**
+* Set attribute Estimate
+*@param bool value: the value to set the attribute to
+*/
+	 void JDFAutoDieLayoutProductionParams::SetEstimate(bool value){
+	SetAttribute(atr_Estimate,WString::valueOf(value));
+};
+/**
+* Get bool attribute Estimate
+* @return bool the vaue of the attribute 
+*/
+	 bool JDFAutoDieLayoutProductionParams::GetEstimate() const {return GetBoolAttribute(atr_Estimate,WString::emptyStr);
+};
+/////////////////////////////////////////////////////////////////////////
+	bool JDFAutoDieLayoutProductionParams::ValidEstimate(EnumValidationLevel level) const {
+		return ValidAttribute(atr_Estimate,AttributeType_boolean,false);
+	};
+///////////////////////////////////////////////////////////////////////
+
+	const WString& JDFAutoDieLayoutProductionParams::PositionString(){
+		static const WString enums=WString(L"Unknown,TopLeft,TopCenter,TopRight,CenterLeft,Center,CenterRight,BottomLeft,BottomCenter,BottomRight");
+		return enums;
+	};
+
+///////////////////////////////////////////////////////////////////////
+
+	WString JDFAutoDieLayoutProductionParams::PositionString(EnumPosition value){
+		return PositionString().Token(value,WString::comma);
+	};
+
+/////////////////////////////////////////////////////////////////////////
+	void JDFAutoDieLayoutProductionParams::SetPosition( EnumPosition value){
+	SetEnumAttribute(atr_Position,value,PositionString());
+};
+/////////////////////////////////////////////////////////////////////////
+	 JDFAutoDieLayoutProductionParams::EnumPosition JDFAutoDieLayoutProductionParams:: GetPosition() const {
+	return (EnumPosition) GetEnumAttribute(atr_Position,PositionString(),WString::emptyStr);
+};
+/////////////////////////////////////////////////////////////////////////
+	bool JDFAutoDieLayoutProductionParams::ValidPosition(EnumValidationLevel level) const {
+		return ValidEnumAttribute(atr_Position,PositionString(),false);
+	};
+
+/* ******************************************************
+// Element Getter / Setter
+**************************************************************** */
+
+
+JDFConvertingConfig JDFAutoDieLayoutProductionParams::GetConvertingConfig(int iSkip)const{
+	JDFConvertingConfig e=GetElement(elm_ConvertingConfig,WString::emptyStr,iSkip);
+	return e;
+};
+/////////////////////////////////////////////////////////////////////
+
+JDFConvertingConfig JDFAutoDieLayoutProductionParams::GetCreateConvertingConfig(int iSkip){
+	JDFConvertingConfig e=GetCreateElement(elm_ConvertingConfig,WString::emptyStr,iSkip);
+	e.init();
+	return e;
+};
+/////////////////////////////////////////////////////////////////////
+
+JDFConvertingConfig JDFAutoDieLayoutProductionParams::AppendConvertingConfig(){
+	JDFConvertingConfig e=AppendElement(elm_ConvertingConfig);
+	e.init();
+	return e;
+};
+/////////////////////////////////////////////////////////////////////
+
+JDFRepeatDesc JDFAutoDieLayoutProductionParams::GetRepeatDesc(int iSkip)const{
+	JDFRepeatDesc e=GetElement(elm_RepeatDesc,WString::emptyStr,iSkip);
+	return e;
+};
+/////////////////////////////////////////////////////////////////////
+
+JDFRepeatDesc JDFAutoDieLayoutProductionParams::GetCreateRepeatDesc(int iSkip){
+	JDFRepeatDesc e=GetCreateElement(elm_RepeatDesc,WString::emptyStr,iSkip);
+	e.init();
+	return e;
+};
+/////////////////////////////////////////////////////////////////////
+
+JDFRepeatDesc JDFAutoDieLayoutProductionParams::AppendRepeatDesc(){
+	JDFRepeatDesc e=AppendElement(elm_RepeatDesc);
+	e.init();
+	return e;
+};
+/////////////////////////////////////////////////////////////////////
+
+/**
+ typesafe validator
+*/
+	vWString JDFAutoDieLayoutProductionParams::GetInvalidElements(EnumValidationLevel level, bool bIgnorePrivate, int nMax) const{
+		int nElem=0;
+		int i=0;
+		vWString vElem=JDFResource::GetInvalidElements(level, bIgnorePrivate, nMax);
+		int n=vElem.size();
+		if(n>=nMax)
+			 return vElem;
+		nElem=NumChildElements(elm_ConvertingConfig);
+		if((level>=ValidationLevel_Complete)&&(nElem<1)) {
+		vElem.AppendUnique(elm_ConvertingConfig);
+			if (++n>=nMax)
+			return vElem;
+		}
+
+		for(i=0;i<nElem;i++){
+			if (!GetConvertingConfig(i).IsValid(level)) {
+				vElem.AppendUnique(elm_ConvertingConfig);
+				if (++n>=nMax)
+					return vElem;
+				break;
+			}
+		}
+		nElem=NumChildElements(elm_RepeatDesc);
+		if((level>=ValidationLevel_Complete)&&(nElem<1)) {
+		vElem.AppendUnique(elm_RepeatDesc);
+			if (++n>=nMax)
+			return vElem;
+		}
+
+		for(i=0;i<nElem;i++){
+			if (!GetRepeatDesc(i).IsValid(level)) {
+				vElem.AppendUnique(elm_RepeatDesc);
+				if (++n>=nMax)
+					return vElem;
+				break;
+			}
+		}
+		return vElem;
+	};
+
+
+/**
+ definition of required elements in the JDF namespace
+*/
+	WString JDFAutoDieLayoutProductionParams::RequiredElements()const{
+		return JDFResource::RequiredElements()+L",ConvertingConfig,RepeatDesc";
+	};
+}; // end namespace JDF
---
+++ src/jdf/wrapper/AutoJDF/JDFAutoDieLayoutProductionParams.h
@@ -0,0 +1,285 @@
+/*
+ * The CIP4 Software License, Version 1.0
+ *
+ *
+ * Copyright (c) 2001-2009 The International Cooperation for the Integration of 
+ * Processes in  Prepress, Press and Postpress (CIP4).  All rights 
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer. 
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution,
+ *    if any, must include the following acknowledgment:  
+ *       "This product includes software developed by the
+ *        The International Cooperation for the Integration of 
+ *        Processes in  Prepress, Press and Postpress (www.cip4.org)"
+ *    Alternately, this acknowledgment may appear in the software itself,
+ *    if and wherever such third-party acknowledgments normally appear.
+ *
+ * 4. The names "CIP4" and "The International Cooperation for the Integration of 
+ *    Processes in  Prepress, Press and Postpress" must
+ *    not be used to endorse or promote products derived from this
+ *    software without prior written permission. For written 
+ *    permission, please contact info@cip4.org.
+ *
+ * 5. Products derived from this software may not be called "CIP4",
+ *    nor may "CIP4" appear in their name, without prior written
+ *    permission of the CIP4 organization
+ *
+ * Usage of this software in commercial products is subject to restrictions. For
+ * details please consult info@cip4.org.
+  *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE INTERNATIONAL COOPERATION FOR
+ * THE INTEGRATION OF PROCESSES IN PREPRESS, PRESS AND POSTPRESS OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the The International Cooperation for the Integration 
+ * of Processes in Prepress, Press and Postpress and was
+ * originally based on software 
+ * copyright (c) 1999-2001, Heidelberger Druckmaschinen AG 
+ * copyright (c) 1999-2001, Agfa-Gevaert N.V. 
+ *  
+ * For more information on The International Cooperation for the 
+ * Integration of Processes in  Prepress, Press and Postpress , please see
+ * <http://www.cip4.org/>.
+ *  
+ * 
+ */
+
+//EndCopyRight
+
+
+///////////////////////////////////////////////////////////////////
+
+ 
+#if !defined _JDFAutoDieLayoutProductionParams_H_
+#define _JDFAutoDieLayoutProductionParams_H_
+#if _MSC_VER >= 1000
+#pragma once
+#endif // _MSC_VER >= 1000
+
+#include "jdf/wrapper/JDFResource.h"
+namespace JDF{
+class JDFConvertingConfig;
+class JDFRepeatDesc;
+class JDFRefElement;
+/*
+*********************************************************************
+class JDFAutoDieLayoutProductionParams : public JDFResource
+
+*********************************************************************
+*/
+/**
+* automatically generated header for JDFAutoDieLayoutProductionParams class
+* 
+* Warning! Do not edit! This file may be regenerated
+* The child Class: @see JDFDieLayoutProductionParams should be edited instead
+*/
+class JDF_WRAPPERCORE_EXPORT JDFAutoDieLayoutProductionParams : public JDFResource{
+public:
+
+
+// Constructors / Destructors  
+
+
+protected:
+/**
+* null ctor
+*/
+	inline JDFAutoDieLayoutProductionParams():JDFResource(){};
+/**
+* copy ctor
+*/
+	inline JDFAutoDieLayoutProductionParams(const KElement & other):JDFResource(){
+	*this=other;
+};
+/**
+* copy equivalance operator
+*/
+	JDFAutoDieLayoutProductionParams &operator =(const KElement& other);
+/**
+* dtor
+*/
+	virtual ~JDFAutoDieLayoutProductionParams(){};
+public:
+
+/**
+* typesafe validator utility
+* @param EnumValidationLevel level validation level
+* @param bool bIgnorePrivate ignore objects in foreign namespaces
+* @param int nMax size of the returned vector
+* @return vWString vector of invalid attribute names
+*/
+	virtual vWString GetInvalidAttributes(EnumValidationLevel level=ValidationLevel_Complete, bool bIgnorePrivate=true, int nMax=9999999)const;
+
+/**
+* typesafe validator utility
+* @param EnumValidationLevel level validation level
+* @param bool bIgnorePrivate ignore objects in foreign namespaces
+* @param int nMax size of the returned vector
+* @return vWString vector of invalid element names
+*/
+	virtual vWString GetInvalidElements(EnumValidationLevel level=ValidationLevel_Complete, bool bIgnorePrivate=true, int nMax=9999999) const;
+
+protected:
+/**
+* typesafe validator utility - list of valid node names for this class 
+ * @return WString& comma separated list of valid node names
+ */
+
+	virtual WString ValidNodeNames()const;
+public:
+
+/**
+* Checks if the node is abstract, i.e. is not completely described
+* utility used by GetUnknownElements, GetUnknownAttributes
+* @return boolean: true, if the node is abstract
+*/
+	virtual bool IsAbstract()const;
+
+
+/* ******************************************************
+// Attribute Getter / Setter
+****************************************************** */
+
+/** 
+ * Typesafe attribute validation of Class
+* @return true if class is valid
+*/
+virtual bool ValidClass(EnumValidationLevel level) const;
+
+/** 
+ * Typesafe initialization
+ * @return true if succcessful
+*/
+virtual bool init();
+
+/**
+* Enumeration for attribute Position
+*/
+
+	enum EnumPosition{Position_Unknown,Position_TopLeft,Position_TopCenter,Position_TopRight,Position_CenterLeft,Position_Center,Position_CenterRight,Position_BottomLeft,Position_BottomCenter,Position_BottomRight};
+
+/**
+ * definition of optional attributes in the JDF namespace
+*/
+	virtual WString OptionalAttributes()const;
+
+/**
+* Set attribute Estimate
+*@param bool value: the value to set the attribute to
+*/
+	virtual void SetEstimate(bool value);
+/**
+* Get bool attribute Estimate
+* @return bool the vaue of the attribute 
+*/
+	virtual bool GetEstimate() const;
+/**
+* Typesafe attribute validation of Estimate
+* @param EnumValidationLevel level of attribute validation 
+* @return bool true if valid
+*/
+	virtual bool ValidEstimate(EnumValidationLevel level=ValidationLevel_Complete) const;
+/**
+* Enumeration strings for Position
+* @return const WString& comma separated list of enumerated string values 
+*/
+	static const WString& PositionString();
+/**
+* Enumeration string for enum value
+* @param EnumPosition value the enumeration to translate
+* @return WString the string representation of the enumeration
+*/
+	static WString PositionString(EnumPosition value);
+/**
+* Set attribute Position
+* @param EnumPosition value the value to set the attribute to
+*/
+	virtual void SetPosition( EnumPosition value);
+
+/**
+* Typesafe enumerated attribute Position
+* @return EnumPositionthe enumeration value of the attribute
+*/
+	virtual EnumPosition GetPosition() const;
+
+/**
+* Typesafe attribute validation of Position
+* @param EnumValidationLevel level element validation level 
+* @return bool true if valid
+*/
+	virtual bool ValidPosition(EnumValidationLevel level=ValidationLevel_Complete) const;
+
+/* ******************************************************
+// Element Getter / Setter
+**************************************************************** */
+
+
+/** Get Element ConvertingConfig
+* 
+* @param int iSkip number of elements to skip
+* @return JDFConvertingConfig The element
+*/
+	JDFConvertingConfig GetCreateConvertingConfig(int iSkip=0);
+
+/**
+* const get element ConvertingConfig
+* @param int iSkip number of elements to skip
+* @return JDFConvertingConfig The element
+*/
+	JDFConvertingConfig GetConvertingConfig(int iSkip=0)const;
+/**
+* Append element ConvertingConfig
+ */
+	JDFConvertingConfig AppendConvertingConfig();
+
+/** Get Element RepeatDesc
+* 
+* @param int iSkip number of elements to skip
+* @return JDFRepeatDesc The element
+*/
+	JDFRepeatDesc GetCreateRepeatDesc(int iSkip=0);
+
+/**
+* const get element RepeatDesc
+* @param int iSkip number of elements to skip
+* @return JDFRepeatDesc The element
+*/
+	JDFRepeatDesc GetRepeatDesc(int iSkip=0)const;
+/**
+* Append element RepeatDesc
+ */
+	JDFRepeatDesc AppendRepeatDesc();
+
+/**
+ definition of required elements in the JDF namespace
+*/
+	virtual WString RequiredElements()const;
+}; // endJDFAutoDieLayoutProductionParams
+
+// ******************************************************
+}; // end namespace JDF
+#endif //_JDFAutoDieLayoutProductionParams_H_
