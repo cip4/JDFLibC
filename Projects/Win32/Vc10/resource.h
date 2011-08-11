e7c52a10d1cbecad02ba8c17538ea17f417e97c3
Alex Khilov <alexkhilov@cip4.org>
2011-08-11 23:35:22 +0000

    JDFC-63 <Culture> changed to 0x0409 (English)
    git-svn-id: http://svn.cip4.org/svnroot/trunk@5392 25ee8a6d-831b-0410-907a-ff8528d65d49

---
+++ Projects/Win32/Vc10/resource.h
@@ -0,0 +1,20 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by JDFToolsLib.rc
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        101
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
+
+#define JDFLIB_VER_STRING "2.1.4a.63"
+#define JDFLIB_VER_MAJOR 2
+#define JDFLIB_VER_MINOR 1
+#define JDFLIB_VER_RELEASE 4
+#define JDFLIB_VER_BUILD 63
