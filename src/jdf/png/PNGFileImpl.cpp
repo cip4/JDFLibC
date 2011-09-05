5df9b5d391a12be1c67d414826a3a6e5a444ecb6
Rainer Prosi <Rainer.Prosi@heidelberg.com>
2011-09-05 13:06:18 +0000

    update build and include directories for pngqueuefilter fixes
    git-svn-id: http://svn.cip4.org/svnroot/trunk@5398 25ee8a6d-831b-0410-907a-ff8528d65d49

--- src/jdf/png/PNGFileImpl.cpp
+++ src/jdf/png/PNGFileImpl.cpp
@@ -1,8 +1,8 @@
 /*
- * The CIP4 Software License, Version 0.1
+ * The CIP4 Software License, Version 1.0
  *
  *
- * Copyright (c) 2001 The International Cooperation for the Integration of 
+ * Copyright (c) 2001-2011 The International Cooperation for the Integration of 
  * Processes in  Prepress, Press and Postpress (CIP4).  All rights 
  * reserved.
  *
